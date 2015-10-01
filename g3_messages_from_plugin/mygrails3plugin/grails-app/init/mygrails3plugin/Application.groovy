bcb598db5cfa8de0921b5832aa9fe1ad3d51d816
Sven Ehrke <sven.ehrke@canoo.com>
2015-10-01 12:42:17 +0200

    initial files

---
+++ g3_messages_from_plugin/mygrails3plugin/grails-app/init/mygrails3plugin/Application.groovy
@@ -0,0 +1,10 @@
+package mygrails3plugin
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
